a34057ca90ea43732ed40d759c6a8d88db2d4283
130FIT <ssboatss03@hotmail.com>
2024-07-03 13:00:30 +0700

    Create entity.BaseEntity

---
+++ src/main/java/com/application/demo_etax_invoice/entity/BaseEntity.java
@@ -0,0 +1,33 @@
+package com.application.demo_etax_invoice.entity;
+
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@Data
+@MappedSuperclass
+public class BaseEntity {
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
