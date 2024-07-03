293d782c742a9b5fb9d1acf895cfecb51bf3ebcb
130FIT <ssboatss03@hotmail.com>
2024-07-03 12:42:15 +0700

    Start Project->compose file->connect database->landing page

---
+++ src/main/java/com/application/demo_etax_invoice/DemoEtaxInvoiceApplication.java
@@ -0,0 +1,13 @@
+package com.application.demo_etax_invoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEtaxInvoiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEtaxInvoiceApplication.class, args);
+    }
+
+}
